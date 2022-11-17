a8648ab2996e16415d05be458622e1357347b21b
Victor Juan <victorjuanrazer@gmail.com>
2022-11-17 14:59:11 -0300

    exercise with methodUse 2 and 3

---
+++ Express/Ex03/metodoUse.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+
+//Metodo use
+server.use(function (req, res, next) {
+  console.log("Inicio...");
+  next();
+  console.log("Fim...");
+});
+
+server.use(function (req, res, next) {
+  console.log("Resposta...");
+  res.send("<h1>Ola APi</h1>");
+});
+
+server.listen(3000, () => [console.log("Executando...")]);
