0f5c821dde1efcdca3e2d050d0b170a9f36c6555
Victor Juan <victorjuanrazer@gmail.com>
2022-11-17 14:48:29 -0300

    exercise 6

---
+++ Fundamentos/Ex06/loadsh.js
@@ -0,0 +1,19 @@
+const _ = require("loadsh");
+
+const alunos = [
+  {
+    nome: "Joao",
+    nota: 7.6,
+  },
+  {
+    nome: "Maria",
+    nota: 8.6,
+  },
+  {
+    nome: "Pedro",
+    nota: 8.1,
+  },
+];
+
+const media = _.sumBy(alunos, "nota") / alunos.length;
+console.log(media);
